8650ec726db77a4eecd4a47ec8ffa8b87295c6ee
Mitch Butler <mitchellbutler@gmail.com>
2022-10-25 12:13:44 -0400

    [feat] removes hover colours

--- src/props/Color.ts
+++ src/props/Color.ts
@@ -4,10 +4,7 @@ import { DefaultTheme } from 'styled-components'
 export interface ColorProps {
   backgroundColor?: keyof DefaultTheme['colors']
   textColor?: keyof DefaultTheme['colors']
-  hoverTextColor?: keyof DefaultTheme['colors']
-  hoverBackgroundColor?: keyof DefaultTheme['colors']
   borderColor?: keyof DefaultTheme['colors']
-  hoverBorderColor?: keyof DefaultTheme['colors']
 }
 
 const getColor = (
@@ -27,43 +24,13 @@ export const getColors = (colorProps: ColorProps) => ({
   theme: DefaultTheme
 }) => {
   const settings = { ...theme.defaults, ...colorProps }
+  const text = getColor(settings.textColor, theme)
+  const background = getColor(settings.backgroundColor, theme)
+  const border = getColor(settings.borderColor, theme)
 
-  const colors = {
-    text: getColor(settings.textColor, theme),
-    background: getColor(settings.backgroundColor, theme),
-    border: getColor(settings.borderColor, theme),
-  }
-
-  let mainStyles = ''
-  if (colors.text) {
-    mainStyles += `color: ${colors.text};`
-  }
-  if (colors.background) {
-    mainStyles += `background-color: ${colors.background};`
-  }
-  if (colors.border) {
-    mainStyles += `border-color: ${colors.border};`
-  }
-
-  const hoverColors = {
-    text: getColor(settings.hoverTextColor, theme),
-    background: getColor(settings.hoverBackgroundColor, theme),
-    border: getColor(settings.hoverBorderColor, theme),
-  }
-
-  let hoverStyles = ''
-  if (hoverColors.text) {
-    hoverStyles += `color: ${hoverColors.text};`
-  }
-  if (hoverColors.background) {
-    hoverStyles += `background-color: ${hoverColors.background};`
-  }
-  if (hoverColors.border) {
-    hoverStyles += `border-color: ${hoverColors.border};`
-  }
-  if (hoverStyles !== '') {
-    hoverStyles = `&:hover {${hoverStyles}}`
-  }
-
-  return [mainStyles, hoverStyles].join(';')
+  return `
+    ${text ? `color: ${text};` : ''}
+    ${background ? `background-color: ${background};` : ''}
+    ${border ? `border-color: ${border};` : ''}
+  `
 }
